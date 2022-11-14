eea727ae12503824d95d62e57bcefbea77226949
marcusFN <marc599n@stud.kea.dk>
2022-11-14 13:57:54 +0100

    Commit with eatMetod

--- src/src/Cat.java
+++ src/src/Cat.java
@@ -5,4 +5,8 @@ public class Cat extends Animal {
     public void makeSound() {
         System.out.println("meow");
     }
+
+    public boolean eat(String foodType) {
+        return true;
+    }
 }
--- src/src/Dog.java
+++ src/src/Dog.java
@@ -5,4 +5,9 @@ public class Dog extends Animal {
     public void makeSound() {
         System.out.println("Wu");
     }
+
+
+    public boolean eat(String foodType) {
+        return true;
+    }
 }
